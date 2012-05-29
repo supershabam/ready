41aa41cb8481323434e910914baab5c6a025e9cf
Ian Hansen <ian@mediarain.com>
2012-05-29 16:00:32 -0600

    allows you to inherit ready without first calling the constructor function (which you still should imo).

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ function Ready() {
 }
 
 Ready.prototype.ready = function(flagOrFunction) {
+  // operate even if inheriting class doesn't properly call constructor
   var ready = !!this._ready
-    , callbacks = this._readyCallbacks || []
+    , callbacks = this._readyCallbacks = this._readyCallbacks || []
     ;
 
   if ('function' === typeof(flagOrFunction)) {
