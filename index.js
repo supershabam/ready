3ada6dc6087e801f0fc726e6713694630cafadce
Ian Hansen <ian@mediarain.com>
2012-05-30 10:45:14 -0600

    makes module a mixin rather than a class

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-function Ready() {
-  this._ready = false;
-  this._readyCallbacks = [];
-}
-
-Ready.prototype.ready = function(flagOrFunction) {
-  // operate even if inheriting class doesn't properly call constructor
-  var ready = !!this._ready
-    , callbacks = this._readyCallbacks = this._readyCallbacks || []
-    ;
+function ready(flagOrFunction) {
+  this._ready = !!this._ready;
+  this._readyCallbacks = this._readyCallbacks || [];
 
   if ('function' === typeof(flagOrFunction)) {
-    callbacks.push(flagOrFunction);
+    this._readyCallbacks.push(flagOrFunction);
   } else {
-    ready = this._ready = !!flagOrFunction;
+    this._ready = !!flagOrFunction;
   }
 
-  if (ready) {
-    callbacks.splice(0, Infinity).forEach(function(callback) {
-      process.nextTick(function() {
-        callback();
-      });
+  if (this._ready) {
+    this._readyCallbacks.splice(0, Infinity).forEach(function(callback) {
+      process.nextTick(callback);
     });
   }
 }
 
-module.exports = Ready;
+exports.mixin = function(object) {
+  object.ready = ready;
+};
--- test/index.js
+++ test/index.js
@@ -1,62 +1,59 @@
-var Ready = require('../')
+var ready = require('../')
   , util = require('util')
   , should = require('should')
   ;
 
 function SomeClass() {
-  Ready.call(this);
   this.property = 'value';
 }
-util.inherits(SomeClass, Ready);
+ready.mixin(SomeClass.prototype);
 SomeClass.prototype.method = function() {
   return 'method';
 };
-
 
 describe('inherits', function() {
   var someClass = new SomeClass();
   it('should have Ready properties', function() {
     someClass.should.have.property('ready');
-    someClass.should.have.property('_ready');
-    someClass.should.have.property('_readyCallbacks');
   });
 
   var anotherClass = new SomeClass();
   it('should be separate from other instances', function() {
     anotherClass.ready(function() {});
+    someClass.ready(function() {});
+    someClass.ready(function() {});
     anotherClass.should.have.property('_readyCallbacks').with.length(1);
-    someClass.should.have.property('_readyCallbacks').with.length(0);
+    someClass.should.have.property('_readyCallbacks').with.length(2);
   });
 });
 
 describe('ready', function() {
-  var ready = new Ready();
+  var someClass = new SomeClass();
   it('should queue callbacks', function() {
-    ready.should.have.property('_readyCallbacks').with.length(0);
-    ready.ready(function() {});
-    ready.should.have.property('_readyCallbacks').with.length(1);
-    ready.ready(function() {});
-    ready.should.have.property('_readyCallbacks').with.length(2);
+    someClass.ready(function() {});
+    someClass.should.have.property('_readyCallbacks').with.length(1);
+    someClass.ready(function() {});
+    someClass.should.have.property('_readyCallbacks').with.length(2);
   });
 
   it('should execute and dequeue callbacks', function(done) {
-    ready.should.have.property('_readyCallbacks').with.length(2);
-    ready.ready(function() {
-      ready.should.have.property('_readyCallbacks').with.length(0);
+    someClass.should.have.property('_readyCallbacks').with.length(2);
+    someClass.ready(function() {
+      someClass.should.have.property('_readyCallbacks').with.length(0);
       done();
     });
-    ready.ready(true);
+    someClass.ready(true);
   });
 
   it('should immediatly call callback when already ready', function(done) {
-    ready.ready(function() {
+    someClass.ready(function() {
       done();
     });
   });
 
   it('should not call when ready set to false', function(done) {
-    ready.ready(false);
-    ready.ready(function(done) {
+    someClass.ready(false);
+    someClass.ready(function(done) {
       done('should not execute because it is not ready');
     });
     setTimeout(function() {
